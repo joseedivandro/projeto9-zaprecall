7effee4ef65322d4ecacf6d6c384f1f7818ed2f5
joseedivandro <jose.edivandro.ja@outlook.com>
2023-03-04 06:50:22 -0300

    colocando botão de escolha

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -11,7 +11,7 @@ import icone_quase from "./assets/icone_quase.png"
 
 export default function Perguntas(props) {
 
-	const { perguntaIndex, cards} = props
+	const { perguntaIndex, cards } = props
 	const [botaoNaoClicado, setbotaoNaoClicado] = useState('')
 	const [botaoClicado, setBotaoClicado] = useState(false)
 	const [icone, setIcone] = useState(seta_play)
@@ -20,10 +20,10 @@ export default function Perguntas(props) {
 
 
 	const iconeMudar = (botaoClicado ? seta_virar :
-		(!botaoNaoClicado ? seta_play :iconeStatus))
+		(!botaoNaoClicado ? seta_play : iconeStatus))
 
 	function fazerQuestao() {
-		
+
 		const botaoStatus = (botaoClicado || botaoNaoClicado)
 		if (botaoStatus) {
 
@@ -32,11 +32,12 @@ export default function Perguntas(props) {
 		botaoClicado ? setBotaoClicado(false) : setBotaoClicado(true);
 	}
 
-	function virarPergunta(){
-		if(!botaoClicado){
+	function virarPergunta() {
+		if (!botaoClicado) {
 			return
-		}else{
+		} else {
 			setQuestaoVirada(true)
+
 		}
 	}
 
@@ -44,13 +45,19 @@ export default function Perguntas(props) {
 
 		<Pergunta botaoClicado={botaoClicado} questaoVirada={questaoVirada} onClick={fazerQuestao}>
 			<Inicio botaoClicado={botaoClicado}>
-				{!botaoClicado ? `pergunta 0${perguntaIndex} ` : (questaoVirada ? cards.answer: cards.question)}
+				{!botaoClicado ? `pergunta 0${perguntaIndex} ` : (questaoVirada ? cards.answer : cards.question)}
 			</Inicio>
 
-			<ImagemInicial
+			<ImagemInicial questaoVirada={questaoVirada}
 				botaoClicado={botaoClicado} > <img src={iconeMudar} alt="imagem" onClick={virarPergunta} />
 
 			</ImagemInicial>
+
+			<BotoesVerdade botaoClicado={botaoClicado} questaoVirada={questaoVirada}>
+				<button className="red">Não lembrei </button>
+				<button className="yellow"> Quase não lembrei</button>
+				<button className="green"> Zap!</button>
+			</BotoesVerdade>
 		</Pergunta>
 
 
@@ -60,17 +67,23 @@ export default function Perguntas(props) {
 
 
 const Pergunta = styled.div`
+
 align-items:center;
 justify-content:space-between;
-display:flex;
+display:${props => props.botaoClicado ? "column" : "flex"};
 margin:auto;
 	margin-bottom: 25px;
     width:300px;
     height:${props => props.botaoClicado ? "131px" : "65px;"};
 	background-color:${props => props.botaoClicado ? "#FFFFD4" : "#FFFFFF"};
 	font-size: 20px;
-	font-weight: 700;S
+	font-weight: 700;
+	border-radius: 5px;
 
+
+	>img{
+		
+	}
 `
 const Inicio = styled.div`
 	
@@ -80,9 +93,39 @@ const Inicio = styled.div`
 `
 
 const ImagemInicial = styled.div`
-height:30px;
+display: ${props => props.questaoVirada ? "none" : "flex"};
+
 
 	img{
 		margin-right: 22px;
+		
 	}
+`
+
+const BotoesVerdade = styled.div`
+
+display:${props => props.questaoVirada ? "flex" : "none"};
+justify-content: space-evenly;
+width: 100%;
+margin-top: 30px;
+
+
+button{
+	width: 85.17px;
+	height: 37.17px;
+	border-radius: 5px;
+	color: white;
+}
+
+.red{
+	background: #FF3030;
+}
+
+.yellow{
+	background: #FF922E;
+}
+
+.green{
+	background: #2FBE34;
+}
 `
